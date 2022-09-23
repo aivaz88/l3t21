a60d9bf11ec6ccf4486432fc1905b650444ba94f
aivaz88 <aivaz1988@mail.ru>
2022-09-23 12:52:16 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите координату Х точки А: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y точки А: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Z точки А: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Х точки B: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y точки B: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Z точки B: ");
+int z2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2, 2)));
